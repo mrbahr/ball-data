dedc9a3d1d5dac1f6e368ee6850a9612d46c3f33
Bahr <justlive_1@hotmail.com>
2026-03-31 15:37:15 +0200

    Add files via upload

--- fetch_matches.py
+++ fetch_matches.py
@@ -2,13 +2,16 @@ import requests
 import json
 import os
 from datetime import datetime
+import pytz # مكتبة ضبط التوقيت
 
-today = datetime.today().strftime('%Y-%m-%d')
+# 1. ضبط التوقيت على توقيت مصر (أو دولتك)
+tz = pytz.timezone('Africa/Cairo')
+today = datetime.now(tz).strftime('%Y-%m-%d')
+
 print(f"Fetching matches for: {today}...\n")
 
 url = "https://v3.football.api-sports.io/fixtures"
 querystring = {"date": today}
-
 
 api_key = os.environ.get("FOOTBALL_API_KEY")
 
@@ -23,7 +26,6 @@ try:
     if response.status_code == 200 and "response" in data:
         matches = data['response']
         
-       
         with open('today_matches.json', 'w', encoding='utf-8') as f:
             json.dump(matches, f, ensure_ascii=False, indent=4)
             
